768e8555b6d05cd74a689f5e49d3644403f1aa5e
Salckoth <00082318@uca.edu.sv>
2023-02-27 08:33:40 -0600

    [ADDED] Display student academic information card

---
+++ lib/widgets/students/academic_record/student_academic_information.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:itpsm_mobile/widgets/utils/vertical_labeled_text.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+class StudentAcademicInformation extends StatelessWidget {
+  const StudentAcademicInformation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: Card(
+        elevation: 4,
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Información académica',
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+              ),
+              const SizedBox(height: 20),
+              ResponsiveRowColumn(
+                rowMainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                rowCrossAxisAlignment: CrossAxisAlignment.start,
+                rowSpacing: 20,
+                columnMainAxisAlignment: MainAxisAlignment.start,
+                columnCrossAxisAlignment: CrossAxisAlignment.center,
+                columnSpacing: 20,
+                layout: ResponsiveWrapper.of(context).isSmallerThan(DESKTOP) ? 
+                ResponsiveRowColumnType.COLUMN : ResponsiveRowColumnType.ROW,
+                children: const [
+                  ResponsiveRowColumnItem(
+                    rowFit: FlexFit.tight,
+                    child: VerticalLabeledText(
+                      label: 'Plan de estudio', 
+                      text: 'Técnico Superior en Hostelería y Turismo'
+                    )
+                  ),
+                  ResponsiveRowColumnItem(
+                    rowFit: FlexFit.tight,
+                    child: VerticalLabeledText(
+                      label: 'Correo institucional', 
+                      text: 'janessa67@yahoo.com'
+                    )
+                  ),
+                  ResponsiveRowColumnItem(
+                    rowFit: FlexFit.tight,
+                    child: VerticalLabeledText(
+                      label: 'Año de ingreso', 
+                      text: '2022'
+                    )
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              ResponsiveRowColumn(
+                rowMainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                rowCrossAxisAlignment: CrossAxisAlignment.start,
+                rowSpacing: 20,
+                columnMainAxisAlignment: MainAxisAlignment.start,
+                columnCrossAxisAlignment: CrossAxisAlignment.center,
+                columnSpacing: 20,
+                layout: ResponsiveWrapper.of(context).isSmallerThan(DESKTOP) ? 
+                ResponsiveRowColumnType.COLUMN : ResponsiveRowColumnType.ROW,
+                children: const [
+                  ResponsiveRowColumnItem(
+                    rowFit: FlexFit.tight,
+                    child: VerticalLabeledText(
+                      label: 'CUM', 
+                      text: '8.90 (mínimo para egresar: 7.00)'
+                    )
+                  ),
+                  ResponsiveRowColumnItem(
+                    rowFit: FlexFit.tight,
+                    child: VerticalLabeledText(
+                      label: 'Estado', 
+                      text: 'En curso'
+                    )
+                  ),
+                  ResponsiveRowColumnItem(
+                    rowFit: FlexFit.tight,
+                    child: VerticalLabeledText(
+                      label: 'UV aprobadas', 
+                      text: '4'
+                    )
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}

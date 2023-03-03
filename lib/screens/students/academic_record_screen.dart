e4abc390420cfb91b91f7c7680d6a8f20ab0e0b1
Salckoth <00082318@uca.edu.sv>
2023-03-02 22:06:47 -0600

    [ADDED] * App bar in academic record screen as mockup * Responsive side drawer menu button

--- lib/screens/students/academic_record_screen.dart
+++ lib/screens/students/academic_record_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:itpsm_mobile/screens/main_drawer/main_drawer_screen.dart';
 import 'package:itpsm_mobile/widgets/students/academic_record/student_academic_information.dart';
 import 'package:itpsm_mobile/widgets/students/academic_record/students_studied_subjects.dart';
+import 'package:responsive_framework/responsive_wrapper.dart';
 
 class AcademicRecordScreen extends StatelessWidget {
   static const routeName = '/academicRecord';
@@ -9,7 +11,25 @@ class AcademicRecordScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+  final ResponsiveWrapperData responsive = ResponsiveWrapper.of(context);
+  
+  return Scaffold(
+      appBar: AppBar(
+        title: const Text('ITPSM'),
+        automaticallyImplyLeading: false,
+        leading: Builder(
+          builder: (BuildContext context) {
+            return responsive.isLargerThan(TABLET) ?
+            const SizedBox() :
+            IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () { Scaffold.of(context).openDrawer(); },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
+        ),
+      ),
+      drawer: const MainDrawerScreen(),
       body: SizedBox.expand(
         child: SingleChildScrollView(
           child: Column(

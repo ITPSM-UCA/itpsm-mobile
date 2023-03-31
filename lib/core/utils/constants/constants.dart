c1ec5af4dc4bcefd2cd69846a3909fc4e0553b8f
Salckoth <00082318@uca.edu.sv>
2023-03-30 20:44:11 -0600

    [ADDED] API Icons mapped as constants

--- lib/core/utils/constants/constants.dart
+++ lib/core/utils/constants/constants.dart
@@ -2,4 +2,9 @@ const String authenticatedUserKey = 'AUTHENTICATED_USER';
 const String localApiPath = 'http://192.168.1.21/api';
 const String dataBaseName = 'itpsm_local_database';
 const String apiName = 'ITPSM API';
+
+// Icons provided by the API
+const String logoutIcon = "logout";
+const String mdSchoolIcon = 'MdSchool';
+
 const Duration responseTimeout = Duration(seconds: 15);

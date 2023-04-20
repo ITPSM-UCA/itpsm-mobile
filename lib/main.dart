29b7879cf4f4a945a14a1f7597ec8438acdb4f09
Slackoth <00082318@uca.edu.sv>
2023-04-19 21:42:01 -0500

    [ADDED] Exception for custom exceptions so they won't hamper integration tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -17,7 +20,13 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import 'features/authentication/presentation/screens/login_screen.dart';
 
-void main() {
+void main() async {
+  FlutterError.onError = (details) {
+    if(details.exception is FlutterError) {
+      FlutterError.presentError(details);
+      if (kReleaseMode) exit(1);
+    }
+  };
   // Ensure the app does not allow landscape mode
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([

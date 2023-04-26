b980977a36ce2abef3b188da2d9f07f69fe2f9e9
Slackoth <00082318@uca.edu.sv>
2023-04-25 22:04:33 -0500

    [ADDED] New sharedPreferences keys as well as a defaultString when object property can't be displayed on the UI

--- lib/core/utils/constants/constants.dart
+++ lib/core/utils/constants/constants.dart
@@ -1,10 +1,14 @@
+// Keys for SharedPreferences objects retrieval
 const String authenticatedUserKey = 'AUTHENTICATED_USER';
-const String localApiPath = 'http://192.168.1.21/api';
-const String dataBaseName = 'itpsm_local_database';
+const String studentsCurriculaKey = 'STUDENTS_CURRICULA';
+const String studentsApprovedSubjectsKey = 'STUDENTS_APPROVED_SUBJECTS_KEY';
+
+const String localApiPath = 'http://172.26.160.1/api';
 const String apiName = 'ITPSM API';
 
 // Icons provided by the API
 const String logoutIcon = "logout";
 const String mdSchoolIcon = 'MdSchool';
 
+const String defaultString = 'No disponible';
 const Duration responseTimeout = Duration(seconds: 15);

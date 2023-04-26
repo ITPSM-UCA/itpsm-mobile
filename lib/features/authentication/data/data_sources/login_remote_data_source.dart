8d4e09fe62930f6131c96f8abcafb7e968d010a5
Slackoth <00082318@uca.edu.sv>
2023-04-25 21:46:34 -0500

    [REFACTORED] Better exception handling with new specific authentication exception

--- lib/features/authentication/data/data_sources/login_remote_data_source.dart
+++ lib/features/authentication/data/data_sources/login_remote_data_source.dart
@@ -5,6 +5,7 @@ import 'package:itpsm_mobile/core/errors/exceptions/http/server_exception.dart';
 import 'package:itpsm_mobile/core/utils/itpsm_utils.dart';
 import 'package:logger/logger.dart';
 
+import '../../../../core/errors/exceptions/authentication/authentication_exception.dart';
 import '../../../../core/utils/constants/constants.dart';
 import '../../../../core/utils/log/get_logger.dart';
 import '../models/authenticated_user_model.dart';
@@ -27,29 +28,20 @@ class LoginRemoteDataSourceImpl extends LoginRemoteDataSource {
   
   @override
   Future<AuthenticatedUserModel> login(String email, String password) async {
-    try {
-      http.Response response = await client.post(path, body: {'email': email, 'password': password})
-        .timeout(responseTimeout, onTimeout: () => http.Response(ItpsmUtils.getTimeoutResponseBody(), 408));
+    http.Response response = await client.post(path, body: {'email': email, 'password': password})
+      .timeout(responseTimeout, onTimeout: () => http.Response(ItpsmUtils.getTimeoutResponseBody(), 408));
 
-      logger.d('Login response body: ${response.body}');
+    logger.d('Login response body: ${response.body}');
 
-      Map<String, dynamic> authenticationResponse = json.decode(response.body);
+    Map<String, dynamic> authenticationResponse = json.decode(response.body);
 
-      if(ItpsmUtils.apiResponseHasErrors(authenticationResponse)) {
-        logger.e('Login error response body: ${json.encode(authenticationResponse)}');
-        // TODO: Pass the error response to the ServerException class and handle it
-        throw ServerException();
-      }
-
-      Map<String, dynamic> user = ItpsmUtils.parseAuthenticationApiResponse(authenticationResponse);
-
-      return AuthenticatedUserModel.fromJson(user);
-    } on ServerException catch(e) {
-      logger.e('Handled error on login.', e);
-      rethrow;
-    } catch (e) {
-      logger.e('Something went wrong when trying to login....', e);
-      throw ServerException();
+    if(ItpsmUtils.apiResponseHasErrors(authenticationResponse)) {
+      logger.e('Login error response body: ${json.encode(authenticationResponse)}');
+      throw AuthenticationException(title: authenticationResponse['errors']['title'], message: authenticationResponse['errors']['detail']);
     }
+
+    Map<String, dynamic> user = ItpsmUtils.parseAuthenticationApiResponse(authenticationResponse);
+
+    return AuthenticatedUserModel.fromJson(user);
   }
 }

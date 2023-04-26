490e409afd78de7c02fb26ebf7e4ac5b08d50d08
Slackoth <00082318@uca.edu.sv>
2023-04-25 22:07:50 -0500

    [ADDED] Academic record repository interface

--- lib/features/students/academic_record/domain/repositories/academic_record_repository.dart
+++ lib/features/students/academic_record/domain/repositories/academic_record_repository.dart
@@ -1,10 +1,10 @@
 import 'package:dartz/dartz.dart';
 import 'package:itpsm_mobile/core/errors/failures/failure.dart';
-import 'package:itpsm_mobile/features/students/academic_record/domain/entities/students_approved_subjects.dart';
+import 'package:itpsm_mobile/features/students/academic_record/data/models/students_approved_subjects_model.dart';
 
 import '../../data/models/students_curricula_model.dart';
 
 abstract class AcademicRecordRepository {
-  Future<Either<Failure, StudentsCurriculaModel>> getStudentsCurricula(int studentId);
-  Future<Either<Failure, List<StudentsApprovedSubjects>>> getStudentsApprovedSubjects();
+  Future<Either<Failure, StudentsCurriculaModel>> getStudentsCurricula(int studentId, String token);
+  Future<Either<Failure, List<StudentsApprovedSubjectsModel>>> getStudentsApprovedSubjects(int studentId, String token);
 }

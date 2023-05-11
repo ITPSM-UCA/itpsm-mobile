bdb6109b082753ae7e5d5b462e5ec6e0c975dc1e
Slackoth <00082318@uca.edu.sv>
2023-05-11 15:45:20 -0500

    [TESTING] AuthenticationRepositoryImpl test ready

--- test/features/authentication/data/repositories/authentication_repository_impl_test.dart
+++ test/features/authentication/data/repositories/authentication_repository_impl_test.dart
@@ -1,8 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:itpsm_mobile/core/errors/exceptions/cache/cache_exception.dart';
-import 'package:itpsm_mobile/core/errors/exceptions/http/server_exception.dart';
-import 'package:itpsm_mobile/core/errors/failures/cache/cache_failure.dart';
+import 'package:itpsm_mobile/core/errors/exceptions/authentication/authentication_exception.dart';
+import 'package:itpsm_mobile/core/errors/failures/authentication/authentication_failure.dart';
 import 'package:itpsm_mobile/core/errors/failures/http/server_failure.dart';
 import 'package:itpsm_mobile/core/network/network_info.dart';
 import 'package:itpsm_mobile/features/authentication/data/data_sources/login_local_data_source.dart';
@@ -27,6 +26,7 @@ void main() {
   late AuthenticationRepository repository;
   late MockNetworkInfo mockNetwork;
   
+  // Initilize mocked dependencies for repository
   setUp(() {
     mockRemoteDataSource = MockLoginRemoteDataSource();
     mockLocalDataSource = MockLoginLocalDataSource();
@@ -50,74 +50,120 @@ void main() {
 
   void runTestsOnline(Function body) {
     group('Device is online', () {
+      // Simulate connection
       setUp(() => when(mockNetwork.isConnected).thenAnswer((_) async => true));
 
       body(); 
     });
   }
 
-  void runTestsOffline(Function body) {
-    group('Device is offline', () {
-      setUp(() => when(mockNetwork.isConnected).thenAnswer((_) async => false));
+  // void runTestsOffline(Function body) {
+  //   group('Device is offline', () {
+  //     // Simulate no connection
+  //     setUp(() => when(mockNetwork.isConnected).thenAnswer((_) async => false));
 
-      body(); 
-    });
-  }
+  //     body(); 
+  //   });
+  // }
 
   runTestsOnline(() {
     test('Should return remote data when the call to remote data source is successful', () async {     
+      // Maps the execution of the mock method and returns a fake response 
       when(mockRemoteDataSource.login(studentEmail, studentPassword))
         .thenAnswer((_) async => dummyAuthUserModel);
 
+      // The mocked method should return what we declared on the "when" function
       final result = await repository.login(studentEmail, studentPassword);
 
+      // Verify the mocked method was called
       verify(mockRemoteDataSource.login(studentEmail, studentPassword));
+
+      // Verify the mocked method returned the same dummy data
       expect(result, equals(const Right(dummyAuthUserModel)));
     });
 
     test('Should cache the data locally when the call to remote data source is successful', () async {     
+      // 1. Simulate login remote data source response
       when(mockRemoteDataSource.login(studentEmail, studentPassword))
         .thenAnswer((_) async => dummyAuthUserModel);
 
+      // 2. Get dummy login data
       await repository.login(studentEmail, studentPassword);
 
+      // 3. Verify the login method from the remote data source was executed
       verify(mockRemoteDataSource.login(studentEmail, studentPassword));
+      
+      // 4. Verify the caching of the dummy login data from the local data source was executed
       verify(mockLocalDataSource.cacheLoginSession(dummyAuthUserModel));
     });
 
-    test('Should return ServerFailure when the call to remote data source is unsuccessful', () async {     
+    test('Should return AuthenticationFailure when the call to remote data source is unsuccessful', () async {     
+      // 1. Simulate login remote data source method throws [AuthenticationException]  when executed
       when(mockRemoteDataSource.login(studentEmail, studentPassword))
-        .thenThrow(ServerException(title: '', message: ''));
+        .thenThrow(const AuthenticationException(title: exceptionTitle, message: exceptionMsg));
 
+      // 2. Get [AuthenticationException]
       final result = await repository.login(studentEmail, studentPassword);
 
+      // 3. Verify the login method from the remote data source was executed
       verify(mockRemoteDataSource.login(studentEmail, studentPassword));
+
+      // Verify that no other method or interaction within the method was executed
+      // 4. Verify the caching of the dummy login data from the local data source was not executed
       verifyZeroInteractions(mockLocalDataSource);
-      expect(result, equals(const Left(ServerFailure(title: '', cause: ''))));
+
+      // 5. Verify the repository returns a [AuthenticationFailure] when login the user
+      expect(result, equals(const Left(AuthenticationFailure(title: failureTitle, cause: failureTitle))));
+    });
+
+    test('Should return ServerFailure when the call to remote data source is unsuccessful', () async {     
+      // 1. Simulate login remote data source exception when executed
+      when(mockRemoteDataSource.login(studentEmail, studentPassword))
+        .thenThrow(Exception());
+
+      // 2. Get [Exception]
+      final result = await repository.login(studentEmail, studentPassword);
+
+      // 3. Verify the login method from the remote data source was executed
+      verify(mockRemoteDataSource.login(studentEmail, studentPassword));
+
+      // 4. Verify the caching of the dummy login data from the local data source was not executed
+      verifyZeroInteractions(mockLocalDataSource);
+
+      // 5. Verify the repository returns a [ServerFailure] when login the user
+      expect(result, equals(const Left(ServerFailure(title: failureTitle, cause: failureTitle))));
     });
   });
 
-  runTestsOffline(() {
-    test('Should return last locally cached data when the cached data is present', () async {
-      when(mockLocalDataSource.getLastLoginSession())
-        .thenAnswer((realInvocation) async => dummyAuthUserModel);
+  // These tests should be done whenever the user enters the app again. Check if user was logged already or not
+  // runTestsOffline(() {
+  //   test('Should return last locally cached data when the cached data is present', () async {
+  //     // 1. Simulate login local data source response
+  //     when(mockLocalDataSource.getLastLoginSession())
+  //       .thenAnswer((realInvocation) async => dummyAuthUserModel);
 
-      final result = await repository.login(studentEmail, studentPassword);
+  //     // 2. Get local dummy login data
+  //     final result = await repository.login(studentEmail, studentPassword);
 
-      verifyZeroInteractions(mockRemoteDataSource);
-      verify(mockLocalDataSource.getLastLoginSession());
-      expect(result, equals(const Right(dummyAuthUserModel)));
-    });
+  //     // 3. Verify no method from the remote data source was executed
+  //     verifyZeroInteractions(mockRemoteDataSource);
+
+  //     // 4. Verify the getLastLoginSession method from the local data source was executed
+  //     verify(mockLocalDataSource.getLastLoginSession());
+
+  //     // 5. Verify the getLastLoginSession method returned the same dummy data
+  //     expect(result, equals(const Right(dummyAuthUserModel)));
+  //   });
     
-    test('Should return CacheFailure when the cached data is not present', () async {
-      when(mockLocalDataSource.getLastLoginSession())
-        .thenThrow(CacheException(title: '', message: ''));
+  //   test('Should return CacheFailure when the cached data is not present', () async {
+  //     when(mockLocalDataSource.getLastLoginSession())
+  //       .thenThrow(const CacheException(title: exceptionTitle, message: exceptionTitle));
 
-      final result = await repository.login(studentEmail, studentPassword);
+  //     final result = await repository.login(studentEmail, studentPassword);
 
-      verifyZeroInteractions(mockRemoteDataSource);
-      verify(mockLocalDataSource.getLastLoginSession());
-      expect(result, equals(const Left(CacheFailure(title: '', cause: ''))));
-    });
-  });
+  //     verifyZeroInteractions(mockRemoteDataSource);
+  //     verify(mockLocalDataSource.getLastLoginSession());
+  //     expect(result, equals(const Left(CacheFailure(title: failureTitle, cause: failureMsg))));
+  //   });
+  // });
 }

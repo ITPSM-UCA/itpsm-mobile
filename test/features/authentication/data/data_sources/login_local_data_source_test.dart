083668559406e9116f55a58fcbe813211639b50d
Slackoth <00082318@uca.edu.sv>
2023-05-11 16:17:00 -0500

    [TESTING] LoginLocalDataSourceImpl test ready

--- test/features/authentication/data/data_sources/login_local_data_source_test.dart
+++ test/features/authentication/data/data_sources/login_local_data_source_test.dart
@@ -1,16 +1,13 @@
 import 'dart:convert';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:itpsm_mobile/core/errors/exceptions/cache/cache_exception.dart';
 import 'package:itpsm_mobile/features/authentication/data/data_sources/login_local_data_source.dart';
-import 'package:itpsm_mobile/features/authentication/data/models/authenticated_user_model.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../../core/utils/constants/constants.dart';
 import '../../../../core/utils/dummies/models/dummy_models.dart';
-import '../../../../fixtures/fixture_reader.dart';
 import 'login_local_data_source_test.mocks.dart';
 
 @GenerateNiceMocks([MockSpec<SharedPreferences>()])
@@ -23,30 +20,31 @@ void main() {
     localDataSource = LoginLocalDataSourceImpl(sharedPreferences: mockSharedPreferences);
   });
 
-  group('getLastLoginSession', () {
-    final authUserModel = AuthenticatedUserModel.fromJson(
-      json.decode(readFixture('authentication', 'login_cache.json'))
-    );
+  // These tests should be done whenever the user enters the app again. Check if user was logged already or not
+  // group('getLastLoginSession', () {
+  //   final authUserModel = AuthenticatedUserModel.fromJson(
+  //     json.decode(readFixture('authentication', 'login_cache.json'))
+  //   );
 
-    test('Should return AuthenticatedUserModel from SharedPreferences when there is one in the cache', () async {
-      when(mockSharedPreferences.getString(authenticatedUserKey))
-        .thenReturn(readFixture('authentication', 'login_cache.json'));
+  //   test('Should return AuthenticatedUserModel from SharedPreferences when there is one in the cache', () async {
+  //     when(mockSharedPreferences.getString(authenticatedUserKey))
+  //       .thenReturn(readFixture('authentication', 'login_cache.json'));
 
-      final result = await localDataSource.getLastLoginSession();
+  //     final result = await localDataSource.getLastLoginSession();
 
-      verify(mockSharedPreferences.getString(authenticatedUserKey));
-      expect(result, authUserModel);
-    });
+  //     verify(mockSharedPreferences.getString(authenticatedUserKey));
+  //     expect(result, authUserModel);
+  //   });
 
-    test('Should throw CacheException when there is not a cached AuthenticatedUserModel', () async {
-      when(mockSharedPreferences.getString(authenticatedUserKey))
-        .thenReturn(null);
+  //   test('Should throw CacheException when there is not a cached AuthenticatedUserModel', () async {
+  //     when(mockSharedPreferences.getString(authenticatedUserKey))
+  //       .thenReturn(null);
 
-      final call = localDataSource.getLastLoginSession;
+  //     final call = localDataSource.getLastLoginSession;
 
-      expect(() => call(), throwsA(const TypeMatcher<CacheException>()));
-    });
-  });
+  //     expect(() => call(), throwsA(const TypeMatcher<CacheException>()));
+  //   });
+  // });
 
   group('cacheLoginSession', () {
     final expectedJson = json.encode(dummyAuthUserModel.toJson());
@@ -59,6 +57,5 @@ void main() {
       verify(mockSharedPreferences.setString(authenticatedUserKey, expectedJson));
       expect(didCache, true);
     });
-    // TODO: Make test for when SharedPreferences setString returns false
   });
 }

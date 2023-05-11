f1178a3de6ddcad6ab52e9210a8e82faf099f7fe
Slackoth <00082318@uca.edu.sv>
2023-05-11 15:41:10 -0500

    [ADDED] Documentacion del metodo

--- test/fixtures/fixture_reader.dart
+++ test/fixtures/fixture_reader.dart
@@ -1,3 +1,5 @@
 import 'dart:io';
 
+
+/// Returns a [String] representation of the [name] file on the specific [directory]
 String readFixture(String directory, String name) => File('test/fixtures/$directory/$name').readAsStringSync();

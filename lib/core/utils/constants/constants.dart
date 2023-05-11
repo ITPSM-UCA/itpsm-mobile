59d4b63ecda2ab45b280916d6040523a4e4947d4
Slackoth <00082318@uca.edu.sv>
2023-05-11 10:24:48 -0500

    [ADDED] SharedPreferences keys for enrollments and evaluations as well as API route names to filter the student's menu

--- lib/core/utils/constants/constants.dart
+++ lib/core/utils/constants/constants.dart
@@ -2,6 +2,8 @@
 const String authenticatedUserKey = 'AUTHENTICATED_USER';
 const String studentsCurriculaKey = 'STUDENTS_CURRICULA';
 const String studentsApprovedSubjectsKey = 'STUDENTS_APPROVED_SUBJECTS_KEY';
+const String studentsEnrollmentsKey = 'STUDENTS_ENROLLMENTS_KEY';
+const String studentsEvaluationsKey = 'STUDENTS_EVALUATIONS_KEY';
 
 const String localApiPath = 'http://172.26.160.1/api';
 const String apiName = 'ITPSM API';
@@ -9,6 +11,10 @@ const String apiName = 'ITPSM API';
 // Icons provided by the API
 const String logoutIcon = "logout";
 const String mdSchoolIcon = 'MdSchool';
+
+// Route names provided by the API
+const String academicRecord = 'Historial academico';
+const String gradesConsultation = 'Ver notas';
 
 const String defaultString = 'No disponible';
 const Duration responseTimeout = Duration(seconds: 15);

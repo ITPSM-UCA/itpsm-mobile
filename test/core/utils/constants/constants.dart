67c61ca498cd989b9a7eb75106c0cceac989ca0b
Slackoth <00082318@uca.edu.sv>
2023-05-28 13:01:28 -0500

    [ADDED] Llaes de SharedPreferences  en el archivo de constants para testing

--- test/core/utils/constants/constants.dart
+++ test/core/utils/constants/constants.dart
@@ -2,7 +2,6 @@ const String adminEmail = 'admin@itpsm.edu.sv';
 const String adminPassword = 'password';
 const String studentEmail = 'janessa67@yahoo.com';
 const String studentPassword = '1234';
-const String authenticatedUserKey = 'AUTHENTICATED_USER';
 const String localApiPath = 'http://172.26.160.1/api';
 const String post = 'POST';
 const String exceptionTitle = 'Exception title';
@@ -11,3 +10,10 @@ const String failureTitle = 'Failure title';
 const String failureMsg = 'Failure message';
 const String token = 'token';
 const int studentId = 1;
+
+// Keys for SharedPreferences objects retrieval
+const String authenticatedUserKey = 'AUTHENTICATED_USER';
+const String studentsCurriculaKey = 'STUDENTS_CURRICULA';
+const String studentsApprovedSubjectsKey = 'STUDENTS_APPROVED_SUBJECTS_KEY';
+const String studentsEnrollmentsKey = 'STUDENTS_ENROLLMENTS_KEY';
+const String studentsEvaluationsKey = 'STUDENTS_EVALUATIONS_KEY';

49d6ee9bb261249334e02d97fc6523f5a2f9f90f
Slackoth <00082318@uca.edu.sv>
2023-05-30 01:53:01 -0500

    [ADDED] Accion de abrir el menu lateral y de cerrar sesion

--- integration_test/robots/test_robot.dart
+++ integration_test/robots/test_robot.dart
@@ -6,8 +6,29 @@ class TestRobot {
 
   final WidgetTester tester;
   final CommonFinders find;
+  final String _logoutDrawerLabel = 'Cerrar Sesión';
 
   const TestRobot({required this.tester, required this.find});
+
+  Future<void> openDrawer() async {
+    final ScaffoldState scaffoldState = tester.firstState(find.byType(Scaffold));
+
+    scaffoldState.openDrawer();
+
+    await waitAndSettle();
+  }
+
+  Future<void> logout() async {
+    await openDrawer();
+
+    final listTile = tester.allWidgets.whereType<ListTile>().firstWhere((listTile) {
+      return (listTile.title as Text) == tester.firstWidget<Text>(find.text(_logoutDrawerLabel));
+    });
+
+    await tester.tap(find.byWidget(listTile));
+
+    await waitAndSettle();
+  }
 
   Future<void> waitAndSettle() async {
     await tester.pumpAndSettle();
